9dac34c3e67d23e238d926451954c4777922627c
Priyanka <priyankapatil1819@gmail.com>
2018-04-15 18:22:14 +0530

    FragmentDemo

---
+++ fragmentjavademo/src/main/java/com/example/fragmentjavademo/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.fragmentjavademo;
+
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        loadFragment(new RedFragment());
+        //loadFragment(new GreenFragment());
+    }
+    public void loadFragment(GreenFragment fragment){
+        FragmentManager manager=getSupportFragmentManager();
+        FragmentTransaction trans=manager.beginTransaction();
+        trans.replace(R.id.framecolor,fragment);
+        trans.commit();
+    }
+
+
+    public void loadFragment(RedFragment fragment) {
+        FragmentManager manager=getSupportFragmentManager();
+        FragmentTransaction trans=manager.beginTransaction();
+        trans.replace(R.id.framecolor,fragment);
+        trans.commit();
+    }
+}
